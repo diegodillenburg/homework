52726033d22ebf3e42bd39403095a6a59d64864c
pedrohcf093 <pedro.hippolito@unesp.br>
2020-10-05 20:34:31 -0300

    Start

--- src/test/java/com/unesp/rc/homework/HomeworkApplicationTests.java
+++ src/test/java/com/unesp/rc/homework/HomeworkApplicationTests.java
@@ -3,10 +3,8 @@ package com.unesp.rc.homework;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class HomeworkApplicationTests {
 
-	@Test
 	void contextLoads() {
 	}
 
