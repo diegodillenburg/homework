afc81dcbe3f2439fa9cdfc8373ab0ab872ebd8f4
Felipe H. Silva <pipocafhs@gmail.com>
2020-10-05 19:46:55 -0300

    start

--- src/test/java/com/unesp/rc/homework/HomeworkApplicationTests.java
+++ src/test/java/com/unesp/rc/homework/HomeworkApplicationTests.java
@@ -1,13 +1,13 @@
-package com.unesp.rc.homework;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class HomeworkApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.unesp.rc.homework;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class HomeworkApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
